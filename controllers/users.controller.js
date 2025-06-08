718fe7d8d2b6e0b52f7108f84cb3f71d80680c4f
anik-das <anikdas1085@gmail.com>
2025-06-08 23:21:19 +0600

    User registration for backend code is completed

---
+++ controllers/users.controller.js
@@ -0,0 +1,68 @@
+import sendEmail from "../config/resendEmail.js";
+import userModel from "../models/user.model.js";
+import bcryptjs from "bcryptjs"
+import verificationEmailTemplate from "../utilities/verifyEmailTemplate.js";
+export const registerUserController =async (req , res)=>{
+    try {
+        const {name , email , password} = req.body ;
+        if (!name || !email || !password)
+            {
+               return res.status(400).json({
+                    message : "Provide name , email and password.",
+                    error : true, 
+                    success : false
+                })
+            } 
+
+        const user = await userModel.findOne({email})
+        
+        if(user)
+        {
+            return res.json({
+                message : "Already register email.",
+                error: true,
+                success: false
+            })
+        }
+    
+    // converting password in bcrypt
+    const salt = await bcryptjs.genSalt(10)
+    const hashPassword = await bcryptjs.hash(password , salt)
+
+    // store in database
+    const payload = {
+        name ,
+        email,
+        password : hashPassword
+    }
+
+    const newUser  = new userModel(payload)
+    const save = await newUser.save();
+
+    const verifyEmailUrl =  `${process.env.FRONTEND_URL}/verify-email?code=${save._id}`;
+
+    const verificationEmail = await sendEmail({
+        sendTo : email,
+        subject : "Verify email from DeshDokan",
+        html : verificationEmailTemplate({
+            name ,
+            url : verifyEmailUrl
+        })
+ 
+    })
+
+    return res.json({
+        message: "Registration successFull. ",
+        error : false ,
+        success : true ,
+        data : save
+    })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
---
+++ utilities/verifyEmailTemplate.js
@@ -0,0 +1,17 @@
+
+const verificationEmailTemplate = (name , url)=>{
+    return 
+    `
+    <h1 style="color: #333333;">Verify Your Email Address</h1>
+    <p style="font-size: 16px; color: #666666;">Hi ${name},</p>
+
+    <p style="font-size: 16px; color: #666666;">Thanks for registering DeshDokan ! Please confirm your email address by clicking the button below:</p>
+    
+    <a href=${url} 
+       style="display: inline-block; margin-top: 20px; padding: 12px 25px; background-color: #007bff; color: #ffffff; text-decoration: none; border-radius: 5px; font-weight: bold;">
+      Verify Email
+    </a>
+    `;
+}
+
+export default verificationEmailTemplate
